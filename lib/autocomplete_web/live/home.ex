95b1b6a438ae5a7f26116fa6ecc7590c0fd51bee
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-13 10:51:21 +0800

    setup form

--- lib/autocomplete_web/live/home.ex
+++ lib/autocomplete_web/live/home.ex
@@ -4,10 +4,14 @@ defmodule AutocompleteWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
+    <form>
+      <input type="text" name="name" value={@name} />
+    </form>
     """
   end
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, name: "")
     {:ok, socket}
   end
 end
