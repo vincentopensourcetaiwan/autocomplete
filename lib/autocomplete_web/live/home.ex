844d3c52f0491a4fb40536e06be40b0b7c6ec31f
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-13 10:54:55 +0800

    setup form change event

--- lib/autocomplete_web/live/home.ex
+++ lib/autocomplete_web/live/home.ex
@@ -4,14 +4,19 @@ defmodule AutocompleteWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
-    <form>
+    <form phx-change="suggest">
       <input type="text" name="name" value={@name} />
     </form>
+    <p><%= @name %></p>
     """
   end
 
   def mount(_params, _session, socket) do
     socket = assign(socket, name: "")
     {:ok, socket}
+  end
+
+  def handle_event("suggest", %{"name" => name}, socket) do
+    {:noreply, assign(socket, name: name)}
   end
 end
